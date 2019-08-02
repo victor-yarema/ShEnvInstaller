94ccc3facf1e98622d119511f34f502a9a084def
Victor Yarema <victor.yarema@gmail.com>
2019-08-02 14:14:14 +0100

    Added ShOptions, OhMyZsh installation(s).

--- Main.sh
+++ Main.sh
@@ -5,13 +5,20 @@ Main() {
 		true
 	) &&
 	InstallShTools 'https://raw.githubusercontent.com/victor-yarema/ShToolsCoreInstaller/master/main.sh?md5=a35be7aa375670baedace672a14c0a99' &&
+	InstallShOptions &&
+	InstallOhMyZsh 'https://raw.githubusercontent.com/victor-yarema/oh-my-zsh/master/tools/install.sh?md5=cff389e35b08e1b7e77fd9fd121e073f' &&
+	exec "${SHELL}" &&
 	true
 } &&
+: &&
+: &&
 Exists() (
 	set -uC &&
 	set -o pipefail &&
 	command -v "$1" >/dev/null 2>&1
 ) &&
+: &&
+: &&
 EnsureTool() (
 	set -uC &&
 	set -o pipefail &&
@@ -38,12 +45,14 @@ EnsureTool() (
 		}
 	}
 ) &&
+: &&
+: &&
 InstallShTools() {
-
 SysDir="${HOME}/.Sys" &&
 (
 set -uC &&
 set -o pipefail &&
+EnsureTool git &&
 EnsureTool curl &&
 Url="$1" &&
 "${SHELL}" -c "$( curl -fsSLv "${Url}" )" \
@@ -74,8 +83,77 @@ AppendText "${HOME}/.bash_profile" \
 true
 ) &&
 . "${SysDir}/ShTools"
-
 } &&
+: &&
+: &&
+InstallShOptions() {
+SysDir="${HOME}/.Sys" &&
+(
+set -uC &&
+set -o pipefail &&
+mkdir -p "${SysDir}" &&
+AppendText "${SysDir}/ShOptions" \
+	'# Shell basics' \
+	"set -uC
+set -o pipefail"$'\n' &&
+AppendText "${SysDir}/StrictShRc" \
+	'# Less options' \
+	"#export LESS='-RSFX -#4'
+export LESS='-RS -#4'
+export LESSCHARSET=utf-8"$'\n' &&
+AppendText "${SysDir}/StrictShRc" \
+	'# Language' \
+	"export LANGUAGE=C.UTF-8"$'\n' &&
+AppendText "${SysDir}/StrictShRc" \
+	'# Make multithreaded' \
+	"alias Make='make -j 8 -O'"$'\n' &&
+true
+) &&
+. "${SysDir}/ShOptions" &&
+. "${SysDir}/StrictShRc"
+} &&
+: &&
+: &&
+InstallOhMyZsh() {
+Url="$1" &&
+SysDir="${HOME}/.Sys" &&
+{
+	! [ -z ${OhMyZshDir+x} ] ||
+	OhMyZshDir="${SysDir}/Tools/OhMyZsh"
+} &&
+AppendText "${SysDir}/OhMyZshCfg" \
+	'# Theme' \
+	$'ZSH_THEME="robbyrussell"\n' &&
+AppendText "${SysDir}/OhMyZshCfg" \
+	'# Plugins' \
+	$'plugins=(git)\n' &&
+AppendText "${SysDir}/OhMyZshCfg" \
+	'# Case sensitive' \
+	$'CASE_SENSITIVE="true"\n' &&
+AppendText "${SysDir}/OhMyZshCfg" \
+	'# Updates' \
+	$'#export UPDATE_ZSH_DAYS=1\n' &&
+AppendText "${SysDir}/OhMyZshCfg" \
+	'# Completion dots' \
+	$'COMPLETION_WAITING_DOTS="true"\n' &&
+AppendText "${SysDir}/OhMyZshCfg" \
+	'# History time format' \
+	$'HIST_STAMPS="yyyy-mm-dd"\n' &&
+AppendText "${SysDir}/OhMyZsh" \
+	'# All' \
+	". '${SysDir}/OhMyZshCfg'
+export OhMyZshDir='${OhMyZshDir}'
+. "'"${OhMyZshDir}/oh-my-zsh.sh"'$'\n' &&
+AppendText "${SysDir}/ZshRc" \
+	'# OhMyZsh' \
+	". '${SysDir}/OhMyZsh'"$'\n' &&
+AppendText "${HOME}/.zshrc" \
+	'# ZshRc' \
+	". '${SysDir}/ZshRc'"$'\n' &&
+"${SHELL}" -c "set -x && set +u && $( curl -fsSLv "${Url}" )"
+} &&
+: &&
+: &&
 Main "$@"
 
 Res=$?
