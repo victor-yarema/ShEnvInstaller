74208aa77eb5dfb3ba26fd4dc70ac4c75ed024d3
Victor Yarema <victor.yarema@gmail.com>
2019-08-02 13:35:27 +0100

    Added ShTools installation.

--- Main.sh
+++ Main.sh
@@ -1,3 +1,12 @@
+Main() {
+	(
+		set -xuC &&
+		set -o pipefail &&
+		true
+	) &&
+	InstallShTools 'https://raw.githubusercontent.com/victor-yarema/ShToolsCoreInstaller/master/main.sh?md5=a35be7aa375670baedace672a14c0a99' &&
+	true
+} &&
 Exists() (
 	set -uC &&
 	set -o pipefail &&
@@ -29,9 +38,51 @@ EnsureTool() (
 		}
 	}
 ) &&
-Main() (
-	set -xuC &&
-	set -o pipefail &&
-	EnsureTool git
+InstallShTools() {
+
+SysDir="${HOME}/.Sys" &&
+(
+set -uC &&
+set -o pipefail &&
+EnsureTool curl &&
+Url="$1" &&
+"${SHELL}" -c "$( curl -fsSLv "${Url}" )" \
+	'' "${HOME}/Tools/ShToolsCore" "${HOME}/Tools/ShToolsText" "${SysDir}" &&
+. "${SysDir}/ShTools" &&
+touch "${SysDir}/UnstrictShRc" &&
+touch "${SysDir}/ShOptions" &&
+AppendText "${SysDir}/StrictShRc" \
+	'# ShTools' \
+	". '${SysDir}/ShTools'"$'\n' &&
+AppendText "${SysDir}/ShRc" \
+	'# All' \
+	". '${SysDir}/UnstrictShRc'
+. '${SysDir}/ShOptions'
+. '${SysDir}/StrictShRc'"$'\n' &&
+AppendText "${SysDir}/ZshRc" \
+	'# ShRc' \
+	". '${SysDir}/ShRc'"$'\n' &&
+AppendText "${HOME}/.zshrc" \
+	'# ZshRc' \
+	". '${SysDir}/ZshRc'"$'\n' &&
+AppendText "${SysDir}/BashRc" \
+	'# ShRc' \
+	". '${SysDir}/ShRc'"$'\n' &&
+AppendText "${HOME}/.bash_profile" \
+	'# BashRc' \
+	". '${SysDir}/BashRc'"$'\n' &&
+true
 ) &&
+. "${SysDir}/ShTools"
+
+} &&
 Main "$@"
+
+Res=$?
+{
+	( exit $Res ) &&
+	echo Success
+} ||
+echo Error
+( exit $Res )
+
